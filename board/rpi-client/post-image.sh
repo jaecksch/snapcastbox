bf6cd677f356523320a27ae40ab20ac9b86e677f
jaecksch <jaecksch@gmx.de>
2018-02-24 19:34:51 +0100

    More work to setup wlan on raspi zero w

--- board/rpi-client/post-image.sh
+++ board/rpi-client/post-image.sh
@@ -11,19 +11,23 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 mkdir "${GENIMAGE_TMP}"
 
+
+cp -rp "${BINARIES_DIR}/rpi-firmware"/* "${GENIMAGE_TMP}"
 cp "${BINARIES_DIR}"/bcm*.dtb "${GENIMAGE_TMP}"
-cp "${BINARIES_DIR}/rpi-firmware/bootcode.bin" "${GENIMAGE_TMP}"
+#cp "${BINARIES_DIR}/rpi-firmware/bootcode.bin" "${GENIMAGE_TMP}"
 cp "${BR2_EXTERNAL_SNAPCASTBOX_PATH}/board/rpi-client/rpi-firmware/cmdline.txt" "${GENIMAGE_TMP}"
 cp "${BR2_EXTERNAL_SNAPCASTBOX_PATH}/board/rpi-client/rpi-firmware/config.txt" "${GENIMAGE_TMP}"
 cp "${BR2_EXTERNAL_SNAPCASTBOX_PATH}/board/rpi-client/rpi-firmware/wpa_supplicant.conf" "${GENIMAGE_TMP}"
-cp "${BINARIES_DIR}/rpi-firmware/fixup.dat" "${GENIMAGE_TMP}"
-cp "${BINARIES_DIR}/rpi-firmware/start.elf" "${GENIMAGE_TMP}"
+#cp "${BINARIES_DIR}/rpi-firmware/fixup.dat" "${GENIMAGE_TMP}"
+#cp "${BINARIES_DIR}/rpi-firmware/start.elf" "${GENIMAGE_TMP}"
 echo snapcastbox >"${GENIMAGE_TMP}/hostname"
 cp "${BINARIES_DIR}/zImage" "${GENIMAGE_TMP}"
 
+cd "${GENIMAGE_TMP}"
+test ! -f "${BINARIES_DIR}/snapcastbox-rpiclient.zip" || rm "${BINARIES_DIR}/snapcastbox-rpiclient.zip"
+zip -vr "${BINARIES_DIR}/snapcastbox-rpiclient" .
 cd "${BINARIES_DIR}"
-zip -vj "${BINARIES_DIR}/snapcastbox-rpiclient" "${GENIMAGE_TMP}"/*
-
 rm -rf "${GENIMAGE_TMP}" 
+
 
 exit $?
