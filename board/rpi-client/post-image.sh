7452e4f1c50e5fe36045b0a977c9898445d54f3e
jaecksch <jaecksch@gmx.de>
2018-03-06 22:42:22 +0100

    Updated post-image.sh to configure audio output according to setup variables chosen in menuconfig

--- board/rpi-client/post-image.sh
+++ board/rpi-client/post-image.sh
@@ -16,12 +16,37 @@ cp -rp "${BINARIES_DIR}/rpi-firmware"/* "${GENIMAGE_TMP}"
 cp "${BINARIES_DIR}"/bcm*.dtb "${GENIMAGE_TMP}"
 cp "${BR2_EXTERNAL_SNAPCASTBOX_PATH}/board/rpi-client/rpi-firmware/cmdline.txt" "${GENIMAGE_TMP}"
 cp "${BR2_EXTERNAL_SNAPCASTBOX_PATH}/board/rpi-client/rpi-firmware/config.txt" "${GENIMAGE_TMP}"
+
+# Patching config.txt according to config variables
+
+if grep -q SNAPCASTBOX_ANALOGUE=y "${BR2_CONFIG}"; then
+
+echo "Forcing Analogue output to config.txt."
+cat << __EOF__ >> "${GENIMAGE_TMP}/config.txt"
+
+# Enable audio (loads snd_bcm2835)
+
+dtparam=audio=on
+hdmi_ignore_edid_audio=1
+__EOF__
+fi
+
+if grep -q SNAPCASTBOX_HB_MINIAMP=y "${BR2_CONFIG}"; then
+	
+echo "Adding Hifiberry MiniAmp Overlay to config.txt."
+cat << __EOF__ >> "${GENIMAGE_TMP}/config.txt"
+
+# Hifiberry MiniAmp
+
+dtoverlay=hifiberry-dac
+__EOF__
+fi
+
 cp "${BR2_EXTERNAL_SNAPCASTBOX_PATH}/board/rpi-client/rpi-firmware/wpa_supplicant.conf" "${GENIMAGE_TMP}"
 #cp "${BINARIES_DIR}/rpi-firmware/fixup.dat" "${GENIMAGE_TMP}"
 #cp "${BINARIES_DIR}/rpi-firmware/start.elf" "${GENIMAGE_TMP}"
 echo snapcastbox >"${GENIMAGE_TMP}/hostname"
 cp "${BINARIES_DIR}/zImage" "${GENIMAGE_TMP}"
-
 cd "${GENIMAGE_TMP}"
 test ! -f "${BINARIES_DIR}/snapcastbox-rpiclient.zip" || rm "${BINARIES_DIR}/snapcastbox-rpiclient.zip"
 zip -vr "${BINARIES_DIR}/snapcastbox-rpiclient" .
